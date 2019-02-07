88474ce2a5f27fdba7eaa0e98caee7c70f361afd
j.kasznia <j.kasznia@o2.pl>
2019-02-07 18:30:31 +0100

    cos

---
+++ IndexersClass/Names.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexersClasses
+{
+    public class Names
+    {
+        private string[] names;
+        public int Count => names.Length; 
+        public Names(int numberOfValues)
+        {
+            names = new string[numberOfValues];
+        }
+
+        public string this[int i]
+        {
+            get
+            {
+                if (i < 0)
+                    throw new IndexOutOfRangeException("Index musi być większy lub równy zero");
+                return names[i];
+            }
+            set
+            {
+                if (i < 0)
+                    throw new IndexOutOfRangeException("Index musi być większy lub równy zero");
+                names[i] = value;
+            }
+        }
+    }
+}
